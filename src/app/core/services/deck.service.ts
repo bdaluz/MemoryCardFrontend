7b98176b2b4c3d6046ed2bc381101d68a0f86c70
Bruno Luz <bdaluzferreira@gmail.com>
2026-01-12 20:54:46 -0300

    feat(deck): separate logic to service and improve UI

--- src/app/core/services/deck.service.ts
+++ src/app/core/services/deck.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Deck } from '../models/deck.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeckService {
-  
+  http = inject(HttpClient);
+  apiUrl = environment.apiUrl;
+
+  getAll() {
+    return this.http.get<Deck[]>(`${this.apiUrl}/decks`);
+  }
 }
--- src/app/features/decks/deck-list/deck-list.ts
+++ src/app/features/decks/deck-list/deck-list.ts
@@ -4,6 +4,7 @@ import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core'
 import { environment } from '../../../../environments/environment';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Observable } from 'rxjs';
+import { DeckService } from '../../../core/services/deck.service';
 
 @Component({
   selector: 'app-deck-list',
@@ -11,18 +12,9 @@ import { Observable } from 'rxjs';
   templateUrl: './deck-list.html',
   styleUrl: './deck-list.css',
 })
-export class DeckList implements OnInit {
-  http = inject(HttpClient);
-  apiUrl = environment.apiUrl;
-  decks$?: Observable<Deck[]>;
-
-  ngOnInit(): void {
-    this.getDecks();
-  }
-
-  getDecks() {
-    this.decks$ = this.http.get<Deck[]>(`${this.apiUrl}/decks`);
-  }
+export class DeckList {
+  deckSvc = inject(DeckService);
+  decks$?: Observable<Deck[]> = this.deckSvc.getAll();
 
   clicked(deck: Deck) {
     console.log('clicked', deck.title);
